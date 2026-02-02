e4c40bb798665c1dc53a5635fcd76d31ef8b602c
Amitav <amitavrchy01@gmail.com>
2026-02-02 17:24:57 +0600

    feat: update releases module to use FormData and S3 for file uploads-2

--- src/main/upload/upload.module.ts
+++ src/main/upload/upload.module.ts
@@ -1,8 +1,10 @@
+import { FileModule } from '@/lib/file/file.module';
 import { Module } from '@nestjs/common';
 import { UploadController } from './upload.controller';
 import { UploadService } from './upload.service';
 
 @Module({
+  imports: [FileModule],
   controllers: [UploadController],
   providers: [UploadService],
   exports: [UploadService],
